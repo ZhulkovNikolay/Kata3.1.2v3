70a3676d2652ab7ad20f816e17a41bf2897e0ce1
ZhulkovNikolay <wed10@yandex.ru>
2024-10-01 17:15:25 +0300

    first

---
+++ src/main/java/com/kata/kata312v3/Kata312v3Application.java
@@ -0,0 +1,13 @@
+package com.kata.kata312v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kata312v3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kata312v3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kata/kata312v3/Kata312v3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.kata312v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kata312v3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
